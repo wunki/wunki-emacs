539a1af04f3aef625992e8f3f21b882f4db4035d
Petar Radosevic <petar@wunki.org>
2018-07-03 22:28:16 +0200

    Configure mu4e for sending email

--- modules/wunki-mu.el
+++ modules/wunki-mu.el
@@ -17,7 +17,8 @@
         mu4e-maildir "~/mail"
         mu4e-context-policy 'pick-first
         mu4e-compose-context-policy nil
-        mu4e-confirm-quit nil)
+        mu4e-confirm-quit nil
+        mu4e-html2text-command "html2text")
   (setq mu4e-contexts
         `( ,(make-mu4e-context
              :name "Personal"
@@ -32,9 +33,15 @@
                      (mu4e-trash-folder      .  "/wunki/trash")
                      (mu4e-refile-folder     .  "/wunki/archive")
                      (mu4e-drafts-folder     .  "/wunki/drafts")
+                     (mu4e-sent-messages-behavior . sent)
                      (mu4e-compose-signature . (concat
                                                 "Petar Radosevic\n"
-                                                "petar@wunki.org"))))
+                                                "petar@wunki.org"))
+                     (starttls-use-gnutls           . t)
+                     (smtpmail-starttls-credentials . '(("smtp.fastmail.com" 587 nil nil)))
+                     (smtpmail-default-smtp-server . "smtp.fastmail.com")
+                     (smtpmail-smtp-server . "smtp.fastmail.com")
+                     (smtpmail-smtp-user . "wunki@fastmail.fm")))
            ,(make-mu4e-context
              :name "Degreed"
              :enter-func (lambda () (mu4e-message "Entering Degreed context"))
@@ -48,8 +55,15 @@
                      (mu4e-trash-folder      .  "/degreed/trash")
                      (mu4e-refile-folder     .  "/degreed/archive")
                      (mu4e-drafts-folder     .  "/degreed/drafts")
+                     (mu4e-sent-messages-behavior . delete)
                      (mu4e-compose-signature . (concat
                                                 "Petar Radosevic\n"
-                                                "petar@degreed.com")))))))
+                                                "petar@degreed.com"))
+                     (starttls-use-gnutls           . t)
+                     (smtpmail-starttls-credentials . '(("smtp.gmail.com" 587 nil nil)))
+                     (smtpmail-default-smtp-server . "smtp.gmail.com")
+                     (smtpmail-smtp-server . "smtp.gmail.com")
+                     (smtpmail-smtp-service . 587)
+                     (smtpmail-smtp-user . "petar@degreed.com"))))))
 
 (provide 'wunki-mu)
