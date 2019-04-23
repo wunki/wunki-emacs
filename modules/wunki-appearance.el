730245608828eb1059c3aba164be582977332a8e
Petar Radosevic <petar@wunki.org>
2019-04-23 09:01:59 +0200

    Switch font for Mac

--- modules/wunki-appearance.el
+++ modules/wunki-appearance.el
@@ -8,7 +8,7 @@
 
 ;; Font configuration and settings
 (defvar wunki/font-choice (cond
-                           ((wunki/is-mac) "Operator Mono-14:weight=Light")
+                           ((wunki/is-mac) "Dank Mono-14")
                            ((wunki/is-windows) "Operator Mono Book-9")
                            (t "OperatorMonoSsmBook-8")))
 
