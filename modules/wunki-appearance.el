5254fbd9e617298474ff7337167540333bf5c761
Petar Radosevic <petar@petar.dev>
2019-08-29 09:16:08 +0200

    Increase font size

--- modules/wunki-appearance.el
+++ modules/wunki-appearance.el
@@ -8,7 +8,7 @@
 
 ;; Font configuration and settings
 (defvar wunki/font-choice (cond
-                           ((wunki/is-mac) "Dank Mono-14")
+                           ((wunki/is-mac) "Dank Mono-15")
                            ((wunki/is-windows) "Operator Mono Book-9")
                            (t "OperatorMonoSsmBook-8")))
 
