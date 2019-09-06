361f3de4c6b789072ba547613dbeb61b30a3ca63
Petar Radosevic <petar@petar.dev>
2019-09-06 07:27:28 +0200

    Minimal look for Emacs

--- modules/wunki-appearance.el
+++ modules/wunki-appearance.el
@@ -8,7 +8,7 @@
 
 ;; Font configuration and settings
 (defvar wunki/font-choice (cond
-                           ((wunki/is-mac) "Space Mono-14")
+                           ((wunki/is-mac) "Triplicate T3-15")
                            ((wunki/is-windows) "Operator Mono Book-9")
                            (t "OperatorMonoSsmBook-8")))
 
@@ -39,7 +39,7 @@
   (progn
     (setq doom-themes-enable-bold nil)
     (setq doom-themes-enable-italic t)
-    (load-theme 'doom-challenger-deep t))
+    (load-theme 'almost-mono-white t))
   :config
   (progn
     (set-face-italic 'font-lock-comment-face t)
@@ -47,11 +47,14 @@
     (doom-themes-neotree-config)
     ))
 
+;; Monochromatic themes
+(use-package almost-mono-themes)
+
 ;; When using daemon mode, the theme is not set correctly
 ;; these methods enable us to quickly reload the theme.
 (defun wunki/reload-theme ()
   (interactive)
-  (load-theme 'doom-challenger-deep t)
+  (load-theme 'almost-mono-white t)
   (set-face-italic 'font-lock-comment-face t)
   (wunki/set-font wunki/font-choice)
   (doom-themes-neotree-config)
