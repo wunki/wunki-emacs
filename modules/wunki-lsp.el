a18930e17ccf4a4714621f15fe659f0197b3ca6f
Petar Radosevic <petar@petar.dev>
2020-10-24 16:36:27 +0200

    Format buffer before saving.

--- modules/wunki-lsp.el
+++ modules/wunki-lsp.el
@@ -5,13 +5,17 @@
 ;; Language modules will add their own lsp setup if this is loaded.
 (use-package lsp-mode
   :commands lsp
+  :hook ((before-save . lsp-format-buffer)
+         (before-save . lsp-organize-imports))
+  :config
+  (setq lsp-completion-provider :capf)
+  (add-to-list 'lsp-language-id-configuration '(zig-mode . "zig"))
+  (lsp-register-client
+   (make-lsp-client
+    :new-connection (lsp-stdio-connection "/Users/petar/Code/zls/zig-cache/bin/zls")
+    :major-modes '(zig-mode)
+    :server-id 'zls))
   :diminish lsp-mode)
-
-(with-eval-after-load "company"
-  (use-package company-lsp
-    :after lsp-mode
-    :config
-    (push 'company-lsp company-backends)))
 
 (use-package lsp-ui
   :after lsp-mode
