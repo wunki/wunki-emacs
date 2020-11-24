fd470f44f840f787e8d33cc19143824066e83975
Petar Radosevic <petar@petar.dev>
2020-11-24 11:13:57 +0100

    Fix Eglot for Elixir.

--- modules/wunki-lsp.el
+++ modules/wunki-lsp.el
@@ -7,7 +7,7 @@
   :hook ((elixir-mode . eglot-ensure)
          (rust-mode . eglot-ensure))
   :config
-  (add-to-list 'eglot-server-programs '(elixir-mode "/Users/petar/Code/elixir-ls/release/language_server.sh"))
+  (add-to-list 'eglot-server-programs '(elixir-mode . ("sh" "/Users/petar/Code/elixir-ls/release/language_server.sh")))
   (setq eglot-confirm-server-initiated-edits nil))
 
 (provide 'wunki-lsp)
