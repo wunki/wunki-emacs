7aae0d7af261a0953cb2f2e3584ac14f8b5b02d5
Petar Radosevic <petar@petar.dev>
2020-11-24 10:21:18 +0100

    Switch everything to eglot.

--- modules/wunki-lsp.el
+++ modules/wunki-lsp.el
@@ -1,24 +1,13 @@
 ;;; -*- lexical-binding: t -*-
 ;;; wunki-lsp.el --- Language Server Protocol support.
 
-;; Basic lsp-mode config.
-;; Language modules will add their own lsp setup if this is loaded.
-(use-package lsp-mode
-  :commands lsp
-  :hook ((before-save . lsp-format-buffer)
-         (before-save . lsp-organize-imports))
+(use-package eglot
+  :ensure t
+  :commands (eglot eglot-ensure)
+  :hook ((elixir-mode . eglot-ensure)
+         (rust-mode . eglot-ensure))
   :config
-  (setq lsp-completion-provider :capf)
-  (add-to-list 'lsp-language-id-configuration '(zig-mode . "zig"))
-  (lsp-register-client
-   (make-lsp-client
-    :new-connection (lsp-stdio-connection "/Users/petar/Code/zls/zig-cache/bin/zls")
-    :major-modes '(zig-mode)
-    :server-id 'zls))
-  :diminish lsp-mode)
-
-(use-package lsp-ui
-  :after lsp-mode
-  :hook (lsp-mode . lsp-ui-mode))
+  (add-to-list 'eglot-server-programs '(elixir-mode "/Users/petar/Code/elixir-ls/release/language_server.sh"))
+  (setq eglot-confirm-server-initiated-edits nil))
 
 (provide 'wunki-lsp)
