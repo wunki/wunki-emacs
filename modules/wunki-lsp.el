d60cfca06fded7d7e5b360c697f47010d5ee6353
Petar Radosevic <petar@petar.dev>
2020-12-25 21:28:10 +0100

    Format buffer before save

--- modules/wunki-lsp.el
+++ modules/wunki-lsp.el
@@ -5,9 +5,10 @@
   :ensure t
   :commands (eglot eglot-ensure)
   :hook ((elixir-mode . eglot-ensure)
-         (rust-mode . eglot-ensure))
+         (rust-mode . eglot-ensure)
+         (before-save . eglot-format-buffer))
   :config
-  (add-to-list 'eglot-server-programs '(elixir-mode . ("sh" "/Users/petar/Code/elixir-ls/release/language_server.sh")))
+  (add-to-list 'eglot-server-programs '(elixir-mode . ("sh" "/home/petar/code/elixir-ls/release/language_server.sh")))
   (setq eglot-confirm-server-initiated-edits nil))
 
 (provide 'wunki-lsp)
