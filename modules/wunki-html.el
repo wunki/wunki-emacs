6e72452f98d81c0569a0057cc910df505cec6288
Petar Radosevic <petar@petar.dev>
2021-02-08 11:48:56 +0100

    Detect the engine yourself.

--- modules/wunki-html.el
+++ modules/wunki-html.el
@@ -7,6 +7,7 @@
   (setq web-mode-markup-indent-offset 2)
   (setq web-mode-code-indent-offset 2)
   (setq web-mode-css-indent-offset 2)
+  (setq web-mode-enable-engine-detection t)
 
   (setq web-mode-enable-auto-pairing t)
   (setq web-mode-enable-auto-expanding t)
