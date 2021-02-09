b1b946e3b426b31c5ff7f9b2abc7d287e9029ebd
Petar Radosevic <petar@petar.dev>
2021-02-09 10:07:46 +0100

    New font and theme. Again?! Yes.

--- modules/wunki-appearance.el
+++ modules/wunki-appearance.el
@@ -10,7 +10,7 @@
 (defvar wunki/font-choice (cond
                            ((wunki/is-mac) "Iosevka-13")
                            ((wunki/is-windows) "Iosevka-14")
-                           (t "Iosevka-17")))
+                           (t "Iosevka Curly-18")))
 
 (defun wunki/set-font (font)
   "makes it a bit easier to set the font"
@@ -39,7 +39,7 @@
   (progn
     (setq doom-themes-enable-bold t)
     (setq doom-themes-enable-italic t)
-    (load-theme 'doom-ayu-mirage t))
+    (load-theme 'doom-horizon t))
   :config
   (progn
     (set-face-italic 'font-lock-comment-face t)
@@ -51,7 +51,7 @@
 ;; these methods enable us to quickly reload the theme.
 (defun wunki/reload-theme ()
   (interactive)
-  (load-theme 'doom-ayu-mirage t)
+  (load-theme 'doom-horizon t)
   (set-face-italic 'font-lock-comment-face t)
   (wunki/set-font wunki/font-choice)
   (doom-themes-neotree-config)
