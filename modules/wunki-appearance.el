50af317f8a5b0d45543d65de13b4b16562b32a27
Petar Radosevic <petar@petar.dev>
2021-03-22 10:27:45 +0100

    Cartograph on Linux it is.

--- modules/wunki-appearance.el
+++ modules/wunki-appearance.el
@@ -10,7 +10,7 @@
 (defvar wunki/font-choice (cond
                            ((wunki/is-mac) "Dank Mono-16")
                            ((wunki/is-windows) "MonoLisa-16")
-                           (t "MonoLisa-16")))
+                           (t "CartographCF-17")))
 
 (defun wunki/set-font (font)
   "Make it a bit easier to set the FONT."
