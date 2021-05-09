a43a45c36bdb700043bd14d81643816d37a4de53
Petar Radosevic <petar@petar.dev>
2021-05-09 07:26:38 +0200

    Start eldoc-box hover mode

--- modules/wunki-eglot.el
+++ modules/wunki-eglot.el
@@ -9,7 +9,8 @@
   (add-to-list 'eglot-server-programs `(elixir-mode . ("sh" ,(expand-file-name "~/.local/share/elixir-ls/release/language_server.sh"))))
   :hook (elixir-mode . eglot-ensure))
 
-(use-package eldoc-box)
+(use-package eldoc-box
+  :hook (eldoc-mode-hook . eldoc-box-hover-mode))
 
 (provide 'wunki-eglot)
 ;;; wunki-eglot ends here
